22d6299809dfed81a0284d8e9e0052827e70e614
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-13 12:04:59 -0500

    Basic setup mapping over each object in the swData array.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,15 +2,17 @@ import React, {useState, useEffect,} from 'react';
 import './App.css';
 import axios from 'axios';
 
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [swData, setSwData] = useState();
+  const [swData, setSwData] = useState([]);
 
   useEffect(function(){
     axios.get(`https://swapi.co/api/people/`)
       .then(function(response){
         console.log("This is the response:",response.data.results)
+        setSwData(response.data.results);
       })
       .catch(function(error){
         console.log("This is the error:", error)
@@ -25,6 +27,21 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+        {swData.map(function(trait, index){
+          return(
+            <div key = {index}>
+              <h1>{trait.name}</h1>
+              <div>
+                <h4>Height: {trait.height}</h4>
+                <h4>Weight: {trait.mass}</h4>
+                <h4>Birth: {trait.birth_year}</h4>
+              </div>  
+            </div>
+          );
+        })}
+
+      </div>
     </div>
   );
 }
